7e0928006520bb86747e49577bd70ea7176ec5b3
tera_ny <haru25haru02@gmail.com>
2021-05-05 22:41:26 +0900

    fix package

--- src/modules/firebase.ts
+++ src/modules/firebase.ts
@@ -1,2 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+
+const app =
+  firebase.apps.length === 0
+    ? firebase.initializeApp({
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+      })
+    : firebase.apps[0];
