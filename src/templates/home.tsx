ca01e158b91d7fb22dbd6cbdb983180c1e5b8a78
tera_ny <haru25haru02@gmail.com>
2021-05-06 15:56:04 +0900

    merge

--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -1,31 +1,52 @@
-import { FC, useCallback, useState } from 'react'
-import authState from '~/stores/auth'
-import { useRecoilValue } from 'recoil'
-import firebase from '~/modules/firebase'
-import 'firebase/functions'
-import {setCookie} from 'nookies'
+import { FC, useCallback, useState } from "react"
+import authState from "~/stores/auth"
+import { useRecoilValue } from "recoil"
+import firebase from "~/modules/firebase"
+import "firebase/auth"
 
 const Template: FC = () => {
   const uid = useRecoilValue(authState)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const signIn = useCallback(()=>{
-    firebase.auth().signInWithEmailAndPassword(email, password);
-  },[email, password])
-  const getToken = useCallback(async ()=>{
-    const res = await firebase.functions("asia-northeast1").httpsCallable("http-sessionCookie")()
-    setCookie(null, "Cloud-CDN-Cookie", res.data.token, { domain: res.data.domain, path: res.data.path, expires: new Date(res.data.expires), })
-  },[])
-  return <>{uid ? <>
-  <button onClick={getToken}>get token</button>
-  <audio  controls>
-    <source src="https://media.orange-juice.app/video/private/XCfWJkbGGvT08umQBaMl5ufL10u2/test.mp3" />
-  </audio>
-   </> : <>
-  <input type="mail" placeholder="mail" onChange={e=>setEmail(e.target.value)} value={email}/>
-  <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)} value={password}/>
-  <button onClick={signIn}>signin</button>
-  </>}</>
-} 
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const signIn = useCallback(() => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  }, [email, password])
+  const getToken = useCallback(async () => {
+    const token = await firebase.auth().currentUser.getIdToken()
+    await fetch("/api/session", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  }, [])
+  return (
+    <>
+      {uid ? (
+        <>
+          <button onClick={getToken}>get token</button>
+          <audio controls>
+            <source src="https://media.orange-juice.app/video/private/XCfWJkbGGvT08umQBaMl5ufL10u2/test.mp3" />
+          </audio>
+        </>
+      ) : (
+        <>
+          <input
+            type="mail"
+            placeholder="mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <button onClick={signIn}>signin</button>
+        </>
+      )}
+    </>
+  )
+}
 
 export default Template
