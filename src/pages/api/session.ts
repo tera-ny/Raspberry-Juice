b924678284e49bd7b3c41d53ac7ecec80b7433f6
tera_ny <haru25haru02@gmail.com>
2021-05-06 16:13:50 +0900

    add middleware

--- src/pages/api/session.ts
+++ src/pages/api/session.ts
@@ -5,15 +5,20 @@ const handler: NextApiHandler = async (req, res) => {
   const response = await fetch(
     "https://asia-northeast1-orange-juice-prod.cloudfunctions.net/http-sessionCookie",
     {
+      credentials: "include",
+      method: "POST",
       headers: {
         Authorization: req.headers.authorization,
         "Content-Type": "application/json",
       },
+      body: JSON.stringify({ data: {} }),
     }
   ).then((response) => response.json())
-  setCookie({ res }, "Cloud-CDN-Cookie", response.token, {
-    path: response.path,
-    maxAge: response.maxAge,
+  console.log(response)
+  setCookie({ res }, "Cloud-CDN-Cookie", response.result.token, {
+    domain: response.result.domain,
+    path: response.result.path,
+    maxAge: response.result.maxAge,
     secure: true,
     httpOnly: true,
   })
