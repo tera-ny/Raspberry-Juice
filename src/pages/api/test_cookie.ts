4bc155c3912679276270238adab9b8c95060a2f5
g4zeru <haru25haru02@gmail.com>
2021-05-30 18:34:59 +0900

    delete test

--- src/pages/api/test_cookie.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiHandler } from "next"
-
-const handler: NextApiHandler = async (req, res) => {
-  console.log(req.cookies["test"])
-  res.statusCode = 200
-  res.end()
-}
-
-export default handler
