8eded067d624559917ca12908540ecb3f7fc6c9e
tera_ny <haru25haru02@gmail.com>
2021-07-12 20:31:44 +0900

    fix auth component

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,27 +1,9 @@
-import { FC, useEffect } from "react"
-import { useRecoilState } from "recoil"
-import authState from "~/stores/auth"
-import firebase from "~/modules/firebase"
+import { FC } from "react"
+import { subscribeAuth } from "~/stores/auth"
 
-interface Props {
-  shouldLoggedIn?: boolean
-}
-
-const Auth: FC<Props> = ({ children, shouldLoggedIn }) => {
-  const [uid, setAuth] = useRecoilState(authState)
-  useEffect(() => {
-    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      setAuth(user?.uid ?? null)
-    })
-    return () => {
-      unsubscribe()
-    }
-  })
-  if (shouldLoggedIn) {
-    return uid ? <>{children}</> : <></>
-  } else {
-    return <>{children}</>
-  }
+const Auth: FC = ({ children }) => {
+  subscribeAuth()
+  return <>{children}</>
 }
 
 export default Auth
