e4170b2ad5f85e53778464478e9d10631c7080fc
tera_ny <haru25haru02@gmail.com>
2021-08-05 23:30:23 +0900

    remove comment

--- src/modules/hooks.ts
+++ src/modules/hooks.ts
@@ -31,13 +31,9 @@ export const useDocumentEvent = <K extends keyof DocumentEventMap>(
   }, deps)
 
 export const usePrevious = <T>(value: T) => {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
   const ref = useRef<T>()
-  // Store current value in ref
   useEffect(() => {
     ref.current = value
-  }, [value]) // Only re-run if value changes
-  // Return previous value (happens before update in useEffect above)
+  }, [value])
   return ref.current
 }
