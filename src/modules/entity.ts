f1d824cdd986ed685fa8c5182b5ba3b9d2c634f9
tera_ny <haru25haru02@gmail.com>
2021-12-22 22:54:33 +0900

    add entity

--- src/modules/entity.ts
+++ src/modules/entity.ts
@@ -1,3 +1,11 @@
+import {
+  DocumentData,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  Timestamp,
+  WithFieldValue,
+} from "firebase/firestore"
+
 export interface Video<Timestamp> {
   owner: string
   title?: string
@@ -5,6 +13,7 @@ export interface Video<Timestamp> {
   poster?: string
   createdAt?: Timestamp
   description?: string
+  isPublished: boolean
   draft: boolean
   state?: "uploaded" | "transcoded"
 }
@@ -16,14 +25,67 @@ export interface SerializableVideo {
   poster: string | null
   createdAtMillis: number | null
   description: string | null
+  isPublished: boolean | null
   draft: boolean
   state?: "uploaded" | "transcoded"
 }
 
-export interface EditingVideo {
-  title: string
-  poster: string
-  url?: string
-  description?: string
-  readonly state: "uploaded" | "transcoded"
+export interface ResizableImage {
+  medium: string
+  small?: string
+  large?: string
+  thumbnail?: string
+}
+
+export type ResizableImageWithID = ResizableImage & {
+  meta: {
+    id: string
+    originRef: string
+  }
+}
+
+export type Image = ResizableImageWithID & {
+  meta: {
+    uploader?: string
+    isPrivate?: boolean
+  }
+}
+
+export interface Profile<TimeStamp> {
+  name: string
+  description: string
+  registeredAt: TimeStamp
+  banner?: ResizableImageWithID
+  isActive: boolean
+  isBanned: boolean
+  icon?: ResizableImageWithID
+  theme: number
+}
+
+export const profileConvertor: FirestoreDataConverter<Profile<Timestamp>> = {
+  toFirestore: (data: WithFieldValue<Profile<Timestamp>>): DocumentData => {
+    return data
+  },
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot<DocumentData>
+  ): Profile<Timestamp> => {
+    const data = snapshot.data()
+    return data as Profile<Timestamp>
+  },
+}
+
+export const imageConvertor: FirestoreDataConverter<Image> = {
+  toFirestore: (data: WithFieldValue<Image>): DocumentData => {
+    return data
+  },
+  fromFirestore: (snapshot: QueryDocumentSnapshot<DocumentData>): Image => {
+    const data = snapshot.data()
+    return data as Image
+  },
+}
+
+export enum Path {
+  Creator = "creators",
+  Image = "images",
+  Content = "contents",
 }
