d402d0a0b486daf19d35bf947740bd9bc9c22ad4
tera_ny <haru25haru02@gmail.com>
2021-12-22 22:54:22 +0900

    add endpoint

---
+++ src/pages/api/contents/[id]/index.ts
@@ -0,0 +1,33 @@
+import { NextApiHandler } from "next"
+import { verifyAuthCookie } from "~/modules/auth/login"
+import crypto from "crypto"
+import api from "~/modules/api/videos/id"
+
+const calculateHash = (data: string) =>
+  crypto.createHash("sha256").update(data).digest("hex")
+
+const handler: NextApiHandler = async (req, res) => {
+  const id = req.query.id
+  if (typeof id !== "string") return res.status(400).end()
+  try {
+    const decoded = await verifyAuthCookie(req)
+    const response = await api(id, decoded.uid)
+    const hash = calculateHash(
+      JSON.stringify({
+        id: id,
+        last: response.lastupdate,
+      })
+    )
+    res.setHeader("ETag", hash)
+    res.send(JSON.stringify(response.content))
+  } catch (error) {
+    if (typeof error === "number") {
+      return res.status(error).end()
+    } else {
+      console.error(error)
+      return res.status(400).end()
+    }
+  }
+}
+
+export default handler
