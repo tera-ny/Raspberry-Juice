d612690b7f2dcb93867f590ce274224913e353a1
tera_ny <haru25haru02@gmail.com>
2022-07-27 01:03:29 +0900

    add test cases

--- frontend/src/__tests__/header.spec.tsx
+++ frontend/src/__tests__/header.spec.tsx
@@ -2,6 +2,9 @@ import * as stories from "~/stories/header.stories";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { composeStory } from "@storybook/react";
+import user from "@testing-library/user-event";
+import * as authStore from "~/stores/auth";
+import { vi } from "vitest";
 
 const cases: [stories: keyof typeof stories][] = [
   ["Default"],
@@ -10,6 +13,9 @@ const cases: [stories: keyof typeof stories][] = [
 ];
 
 describe("Header component", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
   describe.each(cases)("%s", (story) => {
     const Story = composeStory(stories[story], stories.default);
     it("render component", () => {
@@ -27,6 +33,15 @@ describe("Header component", () => {
         const { queryByRole, queryByText } = render(<Story />);
         expect(queryByText("logged in user")).toBeNull();
         expect(queryByRole("button", { name: "Login" })).not.toBeNull();
+      });
+      it("call toggle auth modal request", () => {
+        const toggle = vi.fn();
+        const useToggle = vi.spyOn(authStore, "useToggleAuthModalRequest");
+        useToggle.mockImplementation(() => toggle);
+        const { getByRole } = render(<Story />);
+        expect(useToggle).toHaveBeenCalled();
+        user.click(getByRole("button", { name: "Login" }));
+        expect(toggle).toHaveBeenCalledTimes(1);
       });
     }
   });
